4364b3fc829a7723de0c8a714d8c1e5cf7a3b460
Pauline Lu <paulinemm520@gmail.com>
2020-11-08 01:10:54 -0800

    Added about us page

---
+++ src/About.js
@@ -0,0 +1,18 @@
+import './App.css'
+
+function About() {
+    return (
+        <div id = "aboutus-title">
+            <div>About Us</div>
+            <div id="aboutus-body">
+                We are three students from Northern California who have been indirectly
+                affected by the catastrophic wildfires that have been ravaging California for
+                the past couple of years. We decided to connect this to climate change because
+                we have personally observed the extreme weather patterns and temperature extremes
+                with the worsening of Northern California wildfires.
+            </div>  
+        </div>
+    );
+}
+
+export default About;
