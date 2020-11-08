c9dc4e8501b86548096391e893afbffbfe631d0f
Pauline Lu <paulinemm520@gmail.com>
2020-11-07 21:33:52 -0800

    Fixed render from index to app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-import Prevention from './Prevention';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Prevention />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
